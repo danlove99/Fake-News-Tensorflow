fda801f8df8515ce45f669f8eb1761b7b8eb301b
Dan Love <danlove99@hotmail.co.uk>
2020-07-22 19:27:58 +0000

    implemented LR decay

--- train.py
+++ train.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as np  
 import tensorflow as tf  
 from tensorflow import keras 
+from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.layers import Dense, Embedding, GlobalAveragePooling1D
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.callbacks import LearningRateScheduler
+import math
 
 # Definitions 
 
@@ -39,7 +42,6 @@ for index, row in df.iterrows():
 
 print("Succesfully appended data to array")
 
-
 # Split data for training and testing
 
 training_sentences = sentences[0:training_size]
@@ -52,19 +54,20 @@ testing_labels = labels[training_size:]
 
 tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
 tokenizer.fit_on_texts(training_sentences)
+print("Fit tokenizer on news dataset")
 word_index = tokenizer.word_index
 training_sequences = tokenizer.texts_to_sequences(training_sentences)
 training_padded = pad_sequences(training_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
 testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
 testing_padded = pad_sequences(testing_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
-
+print("Added to padding")
 
 # work with TensorFlow 2
 training_padded = np.array(training_padded)
 training_labels = np.array(training_labels)
 testing_padded = np.array(testing_padded)
 testing_labels = np.array(testing_labels)
-
+print("Converted to numpy arrays")
 # model
 model = Sequential()
 model.add(Embedding(vocab_size, embedding_dim, input_length=max_length))
@@ -72,6 +75,33 @@ model.add(GlobalAveragePooling1D())
 model.add(Dense(24, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
+# LR Decay
+def step_decay(epoch):
+   initial_lrate = 0.1
+   drop = 0.5
+   epochs_drop = 10.0
+   lrate = initial_lrate * math.pow(drop,  
+           math.floor((1+epoch)/epochs_drop))
+   return lrate
+   
+lrate = LearningRateScheduler(step_decay)
+   
+class LossHistory(tf.keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+       self.losses = []
+       self.lr = []
+ 
+    def on_epoch_end(self, batch, logs={}):
+       self.losses.append(logs.get('loss'))
+       self.lr.append(step_decay(len(self.losses)))
+    
+loss_history = LossHistory()
+lrate = LearningRateScheduler(step_decay)
+checkpointer = ModelCheckpoint(filepath="weights.hdf5", verbose=1, save_best_only=True)
+callbacks_list = [loss_history, lrate, checkpointer]
 
 model.compile(optimizer='SGD', loss='binary_crossentropy', metrics=['accuracy'])
-history = model.fit(training_padded, training_labels, epochs=10, validation_data=(testing_padded, testing_labels), verbose=2, batch_size=10)
+
+history = model.fit(training_padded, training_labels, epochs=6,
+					validation_data=(testing_padded, testing_labels)
+					, verbose=2, batch_size=10, callbacks=callbacks_list)
